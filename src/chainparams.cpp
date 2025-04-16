8494b37e301d6f45bf0e61e0a7583b1505b8989b
miner2046 <29011459@qq.com>
2025-04-16 11:17:04 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,7 +70,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         strNetworkID = "main";
-        consensus.nSubsidyHalvingInterval = 210000;
+        consensus.nSubsidyHalvingInterval = 10000;
         consensus.nMajorityEnforceBlockUpgrade = 750;
         consensus.nMajorityRejectBlockOutdated = 950;
         consensus.nMajorityWindow = 1000;
